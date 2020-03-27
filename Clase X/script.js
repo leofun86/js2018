efd9bdef0a0346f690b309ff5b8765b7624bd713
Leo Funes <leorecord@hotmail.com>
2020-03-27 04:04:05 -0300

    Primer subida

---
+++ Clase X/script.js
@@ -0,0 +1,9 @@
+let numero = 4;
+let palabra = 'Hola Mundo';
+let respuesta = false;
+
+let usuario = 'Leandro';
+
+console.log(numero);
+console.log(++numero);
+console.log(`Num1: ${numero--}\nNum2: ${numero}`);
